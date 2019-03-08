491bc9e723ef815d38574eca1566ffc2a05d1fcf
erick <ericksossa09@gmail.com>
2019-03-08 15:00:56 -0500

    agregando contenido en las secciones

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class FooterComponent implements OnInit {
-
+  anio: number = new Date().getFullYear();
   constructor() { }
 
   ngOnInit() {
