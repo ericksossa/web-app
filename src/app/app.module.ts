9fdce359804f1e6405ed50da3e3104d2a191b832
erick <ericksossa09@gmail.com>
2019-03-08 09:11:51 -0500

    estructura del proyecto casi lista faltan las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SectionsComponent } from './shared/sections/sections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SectionsComponent
   ],
   imports: [
     BrowserModule,
